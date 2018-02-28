3eed5561bcaaa8125a93e00cf1b7ff0d8521502e
Oscar Estrada <Oscar.Estrada@TigoUne.com>
2018-02-28 15:16:42 -0500

    ll

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ var express = require('express'),
 Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
+
+app.use('/assets', express.static(__dirname + '/assets'));
+
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
